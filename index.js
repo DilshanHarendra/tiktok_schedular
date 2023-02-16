665a7a140c7a6e94209b205c72a189e467b1a6af
DilshanHarendra <harendracripto@gmail.com>
2023-02-17 01:41:15 +0530

    webhook post route

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app  = express()
-
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.status(200).json({message:'hi'})
 })
 
 app.get('/webhook',(req,res)=>{
     res.status(200).json({message: req.query})
+})
+
+app.post('/webhook',(req,res)=>{
+    res.status(200).json({message: req.body})
 })
 
 app.listen(8000,()=>console.log("app running in 8000"))
