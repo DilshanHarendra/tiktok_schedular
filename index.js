301e00fd964417dae1b161593f3c466e34e8d0e2
DilshanHarendra <harendracripto@gmail.com>
2023-02-17 01:25:16 +0530

    add new routes

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/',(req,res)=>{
     res.status(200).json({message:'hi'})
 })
 
+app.get('/webhook',(req,res)=>{
+    res.status(200).json({message:req})
+})
+
 app.listen(8000,()=>console.log("app running in 8000"))
